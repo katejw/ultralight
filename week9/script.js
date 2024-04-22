808d56ca8aae536a0082a8c9a30afe943072b738
katejw <154714402+katejw@users.noreply.github.com>
2024-04-22 03:28:49 -0400

    Add files via upload

---
+++ week9/script.js
@@ -0,0 +1,36 @@
+// create a function to update the date and time
+function updateDateTime() {
+  var date = new Date();
+  const hour = date.getHours();
+  let status = "";
+
+  var current_time =
+    date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  var current_date =
+    date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
+
+  if (hour >= 6 && hour < 12) {
+    status = "morning";
+    document.getElementById("timeupdate-a").innerHTML = current_time;
+    document.getElementById("dateupdate-a").innerHTML = current_date;
+  } else if (hour >= 12 && hour < 17) {
+    status = "afternoon";
+    document.getElementById("timeupdate-m").innerHTML = current_time;
+    document.getElementById("dateupdate-m").innerHTML = current_date;
+  } else if (hour >= 17 && hour < 21) {
+    status = "evening";
+    document.getElementById("timeupdate-t").innerHTML = current_time;
+    document.getElementById("dateupdate-t").innerHTML = current_date;
+  } else {
+    status = "night";
+    document.getElementById("timeupdate-d").innerHTML = current_time;
+    document.getElementById("dateupdate-d").innerHTML = current_date;
+  }
+
+  document.body.setAttribute("data-status", status);
+}
+
+updateDateTime();
+
+// call the `updateDateTime` function every second
+setInterval(updateDateTime, 1000);
